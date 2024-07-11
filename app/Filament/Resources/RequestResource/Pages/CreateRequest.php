5021879769505dd171a114618059984321f90020
arjunstein <arjun.gnw09@gmail.com>
2024-07-11 22:32:16 +0700

    ✨ feat: add notification request

--- app/Filament/Resources/RequestResource/Pages/CreateRequest.php
+++ app/Filament/Resources/RequestResource/Pages/CreateRequest.php
@@ -3,10 +3,27 @@
 namespace App\Filament\Resources\RequestResource\Pages;
 
 use App\Filament\Resources\RequestResource;
-use Filament\Actions;
+use App\Models\User;
+use Filament\Notifications\Notification;
 use Filament\Resources\Pages\CreateRecord;
 
 class CreateRequest extends CreateRecord
 {
     protected static string $resource = RequestResource::class;
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
+
+    protected function getCreatedNotification(): ?Notification
+    {
+        $recipient = User::all();
+
+        return Notification::make()
+            ->success()
+            ->title('Request created')
+            ->body('The request has been created successfully.')
+            ->sendToDatabase($recipient);
+    }
 }
