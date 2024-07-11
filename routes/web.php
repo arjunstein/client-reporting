1851951a63bc90f7d9453d098ba9c8b6655563fa
arjunstein <arjun.gnw09@gmail.com>
2024-07-11 14:04:00 +0700

    🐛 fixing redirect to relogin after change password

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return redirect('/admin/login');
 });
+
+Route::get('/login', function () {
+    return redirect(route('filament.admin.auth.login'));
+})->name('login');
